9d636ca015ed0804fd2dca5b47e2956712e4fa1c
Evelyne Franco Herrera <evelyne.franco@Evelyne.Franco>
2021-04-28 16:50:49 -0500

    add initial code

---
+++ qa-challenge/Todoist_APP/test/frontend/page_model/pages/loginPage.js
@@ -0,0 +1,19 @@
+import {Selector, t} from 'testcafe'
+
+class loginPage{
+    constructor(){
+        this.emailTextBox=Selector('#email')
+        this.passwordTextBox=Selector('#password')
+        this.loginButton=Selector('.submit_btn')
+        this.errorMessage=Selector('.error_msg')
+    }
+
+    async login(Username, password){
+        await t
+        .typeText(this.emailTextBox, Username)
+        .typeText(this.passwordTextBox, password)
+        .click(this.loginButton)
+    }
+}
+
+export default new loginPage()
---
+++ qa-challenge/Todoist_APP/test/frontend/page_model/test/AddTasks.test.js
@@ -0,0 +1,28 @@
+import todayPage from '../pages/todayPage'
+import {VALID_USER} from '../roles/roles'
+
+
+fixture ('Task feature: add tasks') 
+ .beforeEach(async t=>{
+    await t
+    .useRole(VALID_USER)
+})
+
+test('Validate user is able to add a task', async t=>{
+    await todayPage.addSimpleTask('test')
+    await t.expect(todayPage.task.exists).ok()
+    await t.click(todayPage.checkbox)
+            .wait(5000)
+
+})
+
+test('Validate user is able to add 10 tasks', async t=>{
+    await todayPage.add10Task()
+    await t
+    .expect(todayPage.contentTask.innerText).eql('10')
+    .scroll(todayPage.todayTittle, 'topRight')
+
+    //await t.maximizeWindow()
+    await todayPage.deleteTask()
+    await t.wait(9000)
+})

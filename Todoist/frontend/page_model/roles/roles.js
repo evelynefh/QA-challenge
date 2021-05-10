226e3078d1855a205872c6f5fc4761059f505e33
Evelyne Franco Herrera <evelyne.franco@Evelyne.Franco>
2021-05-09 22:09:40 -0500

    add code changes

---
+++ Todoist/frontend/page_model/roles/roles.js
@@ -0,0 +1,35 @@
+import { Role } from 'testcafe';
+import loginPage from '../pages/loginPage'
+import homePage from '../pages/homePage'
+import { CREDENTIALS } from '../data/Constants'
+
+export const url='https://todoist.com/'
+
+
+export const VALID_USER = Role (url, async t =>{
+    await homePage.submitLoginPage()
+    await loginPage.login(CREDENTIALS.VALID_USER.USERNAME, CREDENTIALS.VALID_USER.PASSWORD)
+    //await t.click(this.timeZone) // when the test is executed uin browserstack timezone alert is displayed, if running in local this line can be deleted or you can change time zone in todoist config page.
+
+},{
+    preserveUrl: true
+});
+
+export const INVALID_EMAIL = Role(url, async t => {
+    await homePage.submitLoginPage()
+
+    await loginPage.login(CREDENTIALS.INVALID_EMAIL.USERNAME, CREDENTIALS.INVALID_EMAIL.PASSWORD)
+    await t.expect(loginPage.errorMessage.exists).ok()
+
+},{
+    preserveUrl: true
+});
+
+export const INVALID_PASSWORD = Role(url, async t => {
+    await homePage.submitLoginPage()
+    await loginPage.login(CREDENTIALS.INVALID_PASSWORD.USERNAME, CREDENTIALS.INVALID_PASSWORD.PASSWORD)
+    await t.expect(loginPage.errorMessage.exists).ok()
+
+},{
+    preserveUrl: true
+});
